533d4871a0b7c93587d50a746f30b9446b7f333b
aselya9185 <58819973+aselya9185@users.noreply.github.com>
2025-03-13 17:25:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ y = st.slider("Y", 0, 130, 10)
 # converting the inputs to json
 inputs = {"operation": option, "x": x, "y": y}
 
+BACKEND_URL = "https://calculator-ceeh.onrender.com"
+
 # when the user clicks on button it will fetch the API
 if st.button("Calculate"):
     res = requests.post(url = "http://127.0.0.1:8000/calculate", data = json.dumps(inputs))
