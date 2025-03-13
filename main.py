515af532eeaf7f0cdad847f60af6ff3453fd22a3
aselya9185 <58819973+aselya9185@users.noreply.github.com>
2025-03-13 17:34:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,6 @@ BACKEND_URL = "https://calculator-ceeh.onrender.com"
 
 # when the user clicks on button it will fetch the API
 if st.button("Calculate"):
-    res = requests.post(url = "http://127.0.0.1:8000/calculate", data = json.dumps(inputs))
+    res = requests.post(url = f"{BACKEND_URL}/calculate", data = json.dumps(inputs))
 
     st.subheader(f"Response from API = {res.text}")
